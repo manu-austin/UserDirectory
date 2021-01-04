1eca448ca8c596558de4779a9cadef4eeba0c55e
Emmanuel Durand <shernua@gmail.com>
2021-01-03 21:41:47 -0600

    testing

---
+++ components/SearchResults.js
@@ -0,0 +1,18 @@
+  
+import React from "react";
+
+function SearchResults() {
+    return (
+        <div>
+            <h1>Hey guys! I'm a header!</h1>
+            <h2>Things I like!</h2>
+            <ul>
+                <li>Peas</li>
+                <li>Carrots</li>
+                <li>Love</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SearchResults;
