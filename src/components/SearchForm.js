482a2ff13b2bdf8fbaa7cce3e2b7d70433e43a2e
Emmanuel Durand <shernua@gmail.com>
2021-01-15 21:18:30 -0600

    adjustments

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,13 +2,14 @@ import React from "react";
 
 
 function SearchForm(props) {
+
     return (
             <form>
                 <div className="form-group">
                     <label htmlFor="search">Search for an Employee:  </label>
 
                     <input
-                        onChange={props.handleInputChange}
+                        onChange={e => props.handleInputChange(e)}
                         value={props.value}
                         name="search"
                         type="text"
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -6,112 +6,133 @@ import "../styles/Result.css";
 
 
 class SearchResult extends Component {
-    
+
     // set initial state
-state = {
-    search: "",
-    filter: "",
-    result: [],
-    sortField: "",
-    filterBy: "lastName"
+    state = {
+        search: "",
+        filter: "",
+        result: [],
+        dataType: [],
+        sortField: "",
+        filterBy: "lastName"
 
-  };
+    };
 
-  componentDidMount() {
-    API.getRandomUser()
-      .then(res => {
-             this.setState({
-            result: res.data.results.map((user, i) => ({
-            firstName: user.name.first,
-            lastName: user.name.last,
-            picture: user.picture.large,
-            email: user.email,
-            phone: user.phone,
-            key: i
-        }))
-
-        })
-     
-      })
-      .catch(err => console.log(err));
-  }
-
-  filterEmployees = () => {
-    if (this.state.result.length === 0) return;
-    const filterResult = this.state.result.filter(el => {
-        if (el.firstName === this.state.search) return true;
-        return false;
-
-    })
-    this.setState({
-        searchResults: filterResult
-    })
-}
-
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.filterEmployees(this.state.search);
-  };
-
-
-
-  handleInputChange = event => {
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState({
-        [name]: value
-    }, ()=> {
-        if (!this.state.search) {
+    apiFunction = () => {
+        API.getRandomUser()
+        .then(res => {
             this.setState({
-                searchResults: this.state.result
+                dataType: res.data.results.map((user, i) => ({
+                    firstName: user.name.first,
+                    lastName: user.name.last,
+                    picture: user.picture.large,
+                    email: user.email,
+                    phone: user.phone,
+                    key: i
+                }))
+
             })
-        }
-    });
-};
+
+        }).then(()=> this.setState({result: this.state.dataType}))
+        .catch(err => console.log(err));
+    }
+
+    componentDidMount() {
+       this.apiFunction();
+       
+    }
+
+    filterSearch = (e) => {
+        let y = this.state.dataType;
+        console.log(e.target.value);
+        let x = e.target.value;
+        const filterResult = y.filter(el => el.firstName.toLowerCase().indexOf(x.toLowerCase()) !== -1)
+            console.log(filterResult);
+            this.setState({
+                result: filterResult
+            })
+    }
+
+    // filterEmployees = (e) => {
+    //     //   console.log(e);
+    //     //   console.log(this.state.result[0].firstName);
+    //     if (this.state.result.length === 0) return;
+    //     // const filterResult = this.state.result.filter(el => {
+    //     //     if (el.firstName === this.state.search) return true;
+    //     //     return false;
+
+    //     // })
+    //     const filterResult = this.state.result.filter(el => el.firstName.toLowerCase().indexOf(e.toLowerCase()) !== -1)
+    //     console.log(filterResult);
+    //     this.setState({
+    //         result: filterResult
+    //     })
+    // };
+
+    // handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     this.filterEmployees(this.state.search);
+    // };
+
+    // handleInputChange = event => {
+    //     event.preventDefault();
+
+    //     const value = event.target.value;
+    //     const name = event.target.name;
+    //     this.setState({
+    //         [name]: value
+    //     }, () => {
+    //         if (!this.state.search) {
+    //             this.setState({
+    //                 searchResults: this.state.result
+    //             })
+    //         }
+    //     });
+    // };
+
+    render() {
 
 
-  render() {
+        return (
 
+            <div className="container">
 
-    return (
+                {/* Title   */}
+                <div className="row">
+                    <h3>Employee Directory</h3>
+                </div>
 
-        <div className="container">
-
-            {/* Title   */}
-            <div className="row">
-                <h3>Employee Directory</h3>
-            </div>
-
-            {/* The line with the form */}
-            <div className="row">    
-                <SearchForm
+                {/* The line with the form */}
+                <div className="row">
+                    {/* <SearchForm
                 value={this.state.search}
                 handleInputChange={this.handleInputChange}
                 handleFormSubmit={this.handleFormSubmit}
-                />
-            </div>
-        
-            {/* Each employee card info */}
-            <div className="row">
-                <table className="table">
-                    {[...this.state.result].map((item) =>
-                    <EmployeeCard
-                        picture={item.picture}
-                        firstName={item.firstName}
-                        lastName={item.lastName}
-                        email={item.email}
-                        phone={item.phone}
-                        key={item.key}
-                    />
-                    )}
-                </table>
-            </div>
+                
+                /> */}
+                    <input type="search" placeholder="Search by First Name" onChange={event => this.filterSearch(event)}/>
+                </div>
+
+                {/* Each employee card info */}
+                <div className="row">
+                    <table className="table">
+                        {[...this.state.result].map((item) =>
+                            <EmployeeCard
+                                picture={item.picture}
+                                firstName={item.firstName}
+                                lastName={item.lastName}
+                                email={item.email}
+                                phone={item.phone}
+                                key={item.key}
+                            />
+                        )}
+                    </table>
+                </div>
 
 
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default SearchResult;
