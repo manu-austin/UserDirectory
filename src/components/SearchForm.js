3c1962b2a26cb9ca78b3e90280bb2a8bdd368f53
Emmanuel Durand <shernua@gmail.com>
2021-01-15 21:20:07 -0600

    adjustments

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,29 @@
-import React from "react";
+// import React from "react";
 
 
-function SearchForm(props) {
+// function SearchForm(props) {
 
-    return (
-            <form>
-                <div className="form-group">
-                    <label htmlFor="search">Search for an Employee:  </label>
+//     return (
+//             <form>
+//                 <div className="form-group">
+//                     <label htmlFor="search">Search for an Employee:  </label>
 
-                    <input
-                        onChange={e => props.handleInputChange(e)}
-                        value={props.value}
-                        name="search"
-                        type="text"
-                        className="form-control"
-                        placeholder="Type a name"
-                        id="search"
-                    />
+//                     <input
+//                         onChange={props.handleInputChange}
+//                         value={props.search}
+//                         name="search"
+//                         type="text"
+//                         className="form-control"
+//                         placeholder="Type a name"
+//                         id="search"
+//                     />
 
-                    <button onClick={props.handleFormSubmit} className="btn">
-                        Search
-                    </button>
-                </div>
-            </form>
-    );
-}
+//                     <button onClick={props.handleFormSubmit} className="btn">
+//                         Search
+//                     </button>
+//                 </div>
+//             </form>
+//     );
+// }
 
-export default SearchForm;
+// export default SearchForm;
